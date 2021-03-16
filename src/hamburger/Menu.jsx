9ddbe009bdafd5f31d0d3dd8da726b71d0ae622c
wanko <kouji124@w4.dion.ne.jp>
2021-03-16 10:40:58 -0700

    indent

--- src/hamburger/Menu.jsx
+++ src/hamburger/Menu.jsx
@@ -22,34 +22,34 @@ const Menu = (props) => {
     }, [props])
     
 
-      const styles={
-        container: {
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          height: open? '100%': 0,
-          width: '100vw',
-          display: 'flex',
-          flexDirection: 'column',
-          background: 'black',
-          opacity: 0.95,
-          color: '#fafafa',
-          transition: 'height 0.3s ease',
-          zIndex: 2,
-        },
-        menuList: {
-          paddingTop: '3rem',
+    const styles={
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: open? '100%': 0,
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        background: 'black',
+        opacity: 0.95,
+        color: '#fafafa',
+        transition: 'height 0.3s ease',
+        zIndex: 2,
+    },
+    menuList: {
+        paddingTop: '3rem',
+    }
+    }
+    return(
+    <div style={styles.container}>
+        {
+        open ?
+            <div style={styles.menuList}> {props.children} </div> : 
+            null
         }
-      }
-      return(
-        <div style={styles.container}>
-          {
-            open ?
-              <div style={styles.menuList}> {props.children} </div> : 
-              null
-          }
-        </div>
-      )
+    </div>
+    )
   }
 
   export default Menu
